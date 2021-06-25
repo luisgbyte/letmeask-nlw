57b85bf0df5a27d626a73377d8003d31b6780b71
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-06-25 18:28:41 -0300

    adding routing and navigation

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom';
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+
 import '../styles/auth.scss';
 
 export function NewRoom() {
@@ -27,7 +30,7 @@ export function NewRoom() {
                         </Button>
                     </form>
                     <p>
-                        Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+                        Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
                     </p>
                 </div>
             </main>
