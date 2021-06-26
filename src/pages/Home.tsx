fa30c3ea938ede7a3b023f3c4d81b71a570e8093
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-06-26 13:32:46 -0300

    authentication context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,25 @@
+import { useContext } from 'react';
 import { useHistory } from 'react-router-dom'
 
-import { auth, firebase } from '../services/firebase';
+import { AuthContext } from '../App';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
+
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext)
 
-  function handleCreateRoom() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    auth.signInWithPopup(provider).then(result => {
-      console.log(result);
-      history.push('/rooms/new');
-    });
+  async function handleCreateRoom() {
+    if(!user) {
+      await signInWithGoogle()
+    }
+    history.push("/rooms/new");
   }
 
     return (
